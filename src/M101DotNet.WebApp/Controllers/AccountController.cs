acf102e748efba1eabfb8eab90a219992284c0e0
sudhirdoke <sudhirdoke@gmail.com>
2016-11-01 20:08:11 -0400

    initial commit

---
+++ src/M101DotNet.WebApp/Controllers/AccountController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using MongoDB.Driver;
+using M101DotNet.WebApp.Models;
+using M101DotNet.WebApp.Models.Account;
+
+namespace M101DotNet.WebApp.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        private BlogContext _blogContext = new BlogContext();
+
+        [HttpGet]
+        public ActionResult Login(string returnUrl)
+        {
+            var model = new LoginModel
+            {
+                ReturnUrl = returnUrl
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            
+            // XXX WORK HERE
+            // fetch a user by the email in model.Email
+            
+            var user = await GetUser(model.Email);
+
+            if (user == null)
+            {
+                ModelState.AddModelError("Email", "Email address has not been registered.");
+                return View(model);
+            }
+
+            var identity = new ClaimsIdentity(new[]
+                {
+                    new Claim(ClaimTypes.Name, user.Name),
+                    new Claim(ClaimTypes.Email, user.Email)
+                },
+                "ApplicationCookie");
+
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignIn(identity);
+
+            return Redirect(GetRedirectUrl(model.ReturnUrl));
+        }
+
+        [HttpPost]
+        public ActionResult Logout()
+        {
+            var context = Request.GetOwinContext();
+            var authManager = context.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View(new RegisterModel());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegisterModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            
+            // XXX WORK HERE
+            // create a new user and insert it into the database
+
+            var user = await GetUser(model.Email);
+
+            if (user == null)
+                await InsertUser(model.Name, model.Email);
+
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        private async Task InsertUser(string name, string email)
+        {
+            await _blogContext.Users.InsertOneAsync(new User { Name = name, Email = email });
+        }
+
+        private async Task<User> GetUser(string email)
+        {
+            
+            var builder = Builders<User>.Filter;
+            var filter = builder.Eq(x => x.Email, email);
+            var result = await _blogContext.Users.Find(filter).ToListAsync();
+            return result.Count > 0 ? result[0] : null;
+
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+    }
+}
